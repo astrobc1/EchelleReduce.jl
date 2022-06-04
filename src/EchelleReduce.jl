bb2e7841e84d8c6e4166721046de8f31121116e5
Bryson Cale <bryson.cale1@gmail.com>
2022-06-04 09:46:18 -0700

    some reorganizing

--- src/EchelleReduce.jl
+++ src/EchelleReduce.jl
@@ -7,18 +7,28 @@ include("recipe.jl")
 include("tracing.jl")
 @reexport using .Tracing
 
+@time begin
 include("precalib.jl")
 @reexport using .PreCalib
+end
 
+@time begin
 include("extract.jl")
 @reexport using .Extract
+end
 
+@time begin
 include("reduction.jl")
+end
 
+@time begin
 include("optimal.jl")
 @reexport using .OptimalExtraction
+end
 
+@time begin
 include("perfectionism.jl")
 @reexport using .SPExtraction
+end
 
 end
